3076453dcd51b6615d8339ecb3b4c7acb91b1f51
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-08-22 11:15:06 -0400

    setup useEffect

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,9 +2,35 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default function Questions() {
+
+    const [theData, setTheData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=10")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.results)
+                setTheData([...data.results])
+            })
+        }, [])
+
+    console.log("I AM theData... " + theData)
+
+    const questions = theData.length > 0
+    ? theData.map(item => {
+        return (
+            <div>
+                <h3>{item.question}</h3>
+                <p>{item.correct_answer}</p>
+            </div>
+        )
+    })
+    : null
+
     return (
         <>
             <h1>QUESTIONS</h1>
+            {questions}
             <Link to="/answers">TAKE ME TO: ANSWERS</Link>
         </>
     )
