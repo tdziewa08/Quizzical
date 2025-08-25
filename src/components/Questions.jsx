5a9d619ffbfd2c1f8fef6bf0ee833b32244abc91
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-08-25 17:39:49 -0400

    update
    added "allAnswers" property to initial fetch request array
    
    added styling

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,27 +1,41 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { decode } from "html-entities"
 
 export default function Questions() {
 
     const [theData, setTheData] = React.useState([])
 
+    function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--)
+        {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    return array;
+}
+
     React.useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=10")
+        fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => {
-                console.log(data.results)
-                setTheData([...data.results])
-            })
-        }, [])
-
-    console.log("I AM theData... " + theData)
+                setTheData(
+                    [...data.results].map(item => {
+                        return {...item, allAnswers : shuffleArray([item.correct_answer, ...item.incorrect_answers])}
+                    }))
+                })
+            }, [])
 
     const questions = theData.length > 0
     ? theData.map(item => {
         return (
-            <div>
-                <h3>{item.question}</h3>
-                <p>{item.correct_answer}</p>
+            <div className="question">
+                <p>{decode(item.question)}</p>
+                <div className="answers">
+                    {item.allAnswers.map(answer => {
+                        return <span>{decode(answer)}</span>
+                    })}
+                </div>
             </div>
         )
     })
@@ -29,8 +43,9 @@ export default function Questions() {
 
     return (
         <>
-            <h1>QUESTIONS</h1>
-            {questions}
+            <div className="questions-container">
+                {questions}
+            </div>
             <Link to="/answers">TAKE ME TO: ANSWERS</Link>
         </>
     )
