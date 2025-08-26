93f45cb82f3a41fd593fa0cf53688730ec3604ab
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-08-26 18:19:53 -0400

    Update Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,6 +5,8 @@ import { decode } from "html-entities"
 export default function Questions() {
 
     const [theData, setTheData] = React.useState([])
+    const [correctAnswers, setTheCorrectAnswers] = React.useState([])
+    console.log("CORRECT ANSWERS: " + correctAnswers)
 
     function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--)
@@ -19,6 +21,7 @@ export default function Questions() {
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => {
+                setTheCorrectAnswers(data.results.map(item => decode(item.correct_answer)))
                 setTheData(
                     [...data.results].map(item => {
                         return {...item, allAnswers : shuffleArray([item.correct_answer, ...item.incorrect_answers])}
@@ -27,13 +30,23 @@ export default function Questions() {
             }, [])
 
     const questions = theData.length > 0
-    ? theData.map(item => {
+    ? theData.map((item, qIdx) => {
         return (
             <div className="question">
                 <p>{decode(item.question)}</p>
                 <div className="answers">
                     {item.allAnswers.map(answer => {
-                        return <span>{decode(answer)}</span>
+                        return (
+                            <label>
+                                <input
+                                    type="radio"
+                                    name={`answer-${qIdx + 1}`}
+                                    value={decode(answer)}
+                                    required
+                                />
+                                {decode(answer)}
+                            </label>
+                        )
                     })}
                 </div>
             </div>
@@ -41,12 +54,38 @@ export default function Questions() {
     })
     : null
 
+    function checkAnswers(e) {
+        e.preventDefault()
+        console.log("Submitted but not refreshed")
+        const form = e.target
+        const formData = new FormData(form)
+        const answer1 = formData.get("answer-1")
+        const answer2 = formData.get("answer-2")
+        const answer3 = formData.get("answer-3")
+        const answer4 = formData.get("answer-4")
+        const answer5 = formData.get("answer-5")
+        const userAnswers = Array.from(formData.values())
+        console.log("USER ANSWERS: " + userAnswers)
+
+        for(let i = 0; i < correctAnswers.length; i++)
+        {
+            if(userAnswers[i] === correctAnswers[i])
+            {
+                console.log("CORRECT DUING DING DING")
+            }
+        }
+    }
+
     return (
         <>
-            <div className="questions-container">
+            <form className="questions-container" onSubmit={checkAnswers}>
                 {questions}
-            </div>
+                <button>Check Your Answers</button>
+            </form>
             <Link to="/answers">TAKE ME TO: ANSWERS</Link>
         </>
     )
 }
+
+//I need to find out what properties I need to use in order to signal to the user and JS to switch a value and have the UI change
+//Current for loop kind of works but only for checking if every answer is correct
